ddb945643579198e62a46562d6d0c176f58f348f
sbmsr <sebastian.messier@gmail.com>
2023-03-14 15:15:08 -0400

    tests updat

--- test.js
+++ test.js
@@ -1,26 +1,96 @@
-import { JSDOM } from "jsdom";
+import { chromium } from "playwright";
 import { setTimeout } from "timers/promises";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
-let filePath = "./src/index.html";
 
-// Helpers
+let browser;
+let context;
+let page;
 
-const hasAllClasses = (dom, id, classes) =>
-  classes.every((val) => dom.window.document.getElementById(id).getAttribute("class").split(" ").includes(val));
-
-const click = (dom, selector) =>
-  dom.window.document.querySelector(selector).dispatchEvent(new dom.window.MouseEvent("click"));
-
-// Tests
-
-test("simple test", async () => {
-  const dom = await JSDOM.fromFile(filePath, {
-    runScripts: "dangerously",
-    resources: "usable",
+test.before(async () => {
+  browser = await chromium.launch({
+    use: { timezoneId: "Etc/UTC" },
   });
-  await setTimeout(10); // let css load
-  assert.is(dom.window.document.querySelector("h1").innerHTML, "Coupon Code Submission Websites");
+  context = await browser.newContext();
+});
+
+test.before.each(async () => {
+  page = await context.newPage();
+});
+
+test.after.each(async () => {
+  await page.close();
+});
+
+test.after(async () => {
+  await browser.close();
+  await context.close();
+});
+
+test("Solved Issue #1: Company Names are Present", async () => {
+  await page.goto("http://localhost:8080");
+  await setTimeout(250);
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(1)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "Fusion LLC");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(1)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "Techopolis Ltd.");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(1)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "Code learning LLC");
+});
+
+test("Solved Issue #2: display dates in 24-hour time format", async () => {
+  await page.goto("http://localhost:8080");
+  await setTimeout(250);
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(3)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "03:41");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(3)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "08:45");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(3)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "12:45");
+});
+
+test("Solved Issue #3: display revenue numbers in a human readable format", async () => {
+  await page.goto("http://localhost:8080");
+  await setTimeout(250);
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(4)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "17 000 000");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(4)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "7 375 294");
+
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(4)", (el) => el.textContent);
+  assert.type(text, "string");
+  assert.is(text, "100 000");
+});
+
+test("Solved Issue #4: make table look prettier", async () => {
+  await page.goto("http://localhost:8080");
+  await setTimeout(250);
+
+  var headerColor = await page.$eval("tr:first-of-type", (el) =>
+    getComputedStyle(el).getPropertyValue("background-color")
+  );
+  assert.is(headerColor, "rgb(173, 216, 230)");
+
+  var tableBorderColor = await page.$eval("body > table", (el) =>
+    getComputedStyle(el).getPropertyValue("border-color")
+  );
+  assert.is(tableBorderColor, "rgb(173, 216, 230)");
 });
 
 test.run();
